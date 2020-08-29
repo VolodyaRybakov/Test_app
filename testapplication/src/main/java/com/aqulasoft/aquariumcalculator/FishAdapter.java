ca683a8671f6535a6b57c4c35af6cc111e138cbb
Woipot <frostik0409@gmail.com>
2020-08-29 20:05:31 +0300

    update list syle

--- testapplication/src/main/java/com/aqulasoft/aquariumcalculator/FishAdapter.java
+++ testapplication/src/main/java/com/aqulasoft/aquariumcalculator/FishAdapter.java
@@ -1,9 +1,11 @@
 package com.aqulasoft.aquariumcalculator;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,12 +13,14 @@ import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class FishAdapter extends RecyclerView.Adapter<FishAdapter.FishViewHolder> {
 
+    private final Random mRandom;
     private LayoutInflater inflater;
     private ArrayList<Fish> fishes;
     private RemoveClickListener mListener;
@@ -24,7 +28,8 @@ public class FishAdapter extends RecyclerView.Adapter<FishAdapter.FishViewHolder
     public FishAdapter(Context context, ArrayList<Fish> fishes, FishListFragment listener){
         this.fishes = fishes;
         this.inflater = LayoutInflater.from(context);
-        mListener=listener;
+        mListener = listener;
+        mRandom = new Random();
     }
 
     @NonNull
@@ -34,12 +39,25 @@ public class FishAdapter extends RecyclerView.Adapter<FishAdapter.FishViewHolder
         return new FishViewHolder(view);
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
     public void onBindViewHolder(@NonNull FishViewHolder holder, int position) {
         Fish fish = fishes.get(position);
+
+        int r = mRandom.nextInt(3);
+        switch (r) {
+            case 1:
+                holder.mImage.setImageResource(R.drawable.ic_fish_2);
+                break;
+            case 2:
+                holder.mImage.setImageResource(R.drawable.ic_fish_3);
+                break;
+            default:
+                holder.mImage.setImageResource(R.drawable.ic_fish_1);
+        }
+
         holder.mFishNameView.setText(fish.getName());
-        holder.mFishHeightView.setText(String.valueOf(fish.getHeight()));
-        holder.mFishLengthView.setText(String.valueOf(fish.getLength()));
+        holder.mFishSizeTextView.setText(String.format("%.0f x %.0f", fish.getHeight(), fish.getLength()));
         holder.mFishKindView.setText(fish.getKind());
     }
 
@@ -55,17 +73,16 @@ public class FishAdapter extends RecyclerView.Adapter<FishAdapter.FishViewHolder
 
     public class FishViewHolder extends RecyclerView.ViewHolder {
 
-        @BindView(R.id.name)
+        @BindView(R.id.itemFishNameTextView)
         TextView mFishNameView;
-        @BindView(R.id.height)
-        TextView mFishHeightView;
-        @BindView(R.id.length)
-        TextView mFishLengthView;
-        @BindView(R.id.kind)
+        @BindView(R.id.itemFishSizeContentTexView)
+        TextView mFishSizeTextView;
+        @BindView(R.id.itemFishKindContentTexView)
         TextView mFishKindView;
         @BindView(R.id.deleteButton)
         AppCompatImageButton mDeleteButton;
-
+        @BindView(R.id.itemFishImage)
+        ImageView mImage;
 
         public FishViewHolder(View view) {
             super(view);
